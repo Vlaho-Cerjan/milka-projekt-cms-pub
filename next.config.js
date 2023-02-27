b525900a3f697316b9dbee037776e1d4d60c908c
Vlaho Cerjan <vlahocerjan9@gmail.com>
2023-02-27 21:18:04 +0100

    Adding cors

--- next.config.js
+++ next.config.js
@@ -26,10 +26,16 @@ export default withPlaiceholder(
       return [
         {
           // matching all API routes (prefixed with /api and https://milka-projekt-api.vercel.app/)
-          source: [
-            "/api/:path*",
-            "https://milka-projekt-api.vercel.app/api/:path*",
-          ],
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        },
+        {
+          source: "https://milka-projekt-api.vercel.app/api/:path*",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
             { key: "Access-Control-Allow-Origin", value: "*" },
