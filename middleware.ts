4e2e59eb2546d5febb784529a1b090bbb4483792
Vlaho Cerjan <vlahocerjan9@gmail.com>
2023-02-28 00:26:56 +0100

    fixing cors 14

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,16 @@
 export { default } from "next-auth/middleware"
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - auth (NextAuth.js routes)
+         * - reset (NextAuth.js routes)
+         * - prisma (Prisma routes)
+         * - static (static files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!api|auth|reset|prisma|static|favicon.ico|auth|reset).*)',
+    ],
+}
