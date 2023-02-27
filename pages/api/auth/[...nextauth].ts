042bd28664af0dd420fcb79b1a88d464f3de1c42
Vlaho Cerjan <vlahocerjan9@gmail.com>
2023-02-27 20:15:24 +0100

    Fixing login url

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -34,8 +34,8 @@ export const authOptions = {
                 // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
                 // You can also use the `req` object to obtain additional parameters
                 // (i.e., the request IP address)
-
-                const res = await fetch(urlServer + "/api/auth/login", {
+                // fetch with current base url pathname
+                const res = await fetch(process.env.NEXT_PUBLIC_LOGIN_URL + "/api/auth/login", {
                     method: 'POST',
                     body: JSON.stringify(credentials),
                     headers: { "Content-Type": "application/json" }
